316e03a8e3a608ce5a847b5ca03b8e279e0acc46
ravi kant <ravikant@sameers-imac.local>
2015-09-22 14:22:42 +0530

    Initial Commit

---
+++ QBSampleChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QBSampleChat
+//
+//  Created by ravi kant on 9/22/15.
+//  Copyright © 2015 Net Solutions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
